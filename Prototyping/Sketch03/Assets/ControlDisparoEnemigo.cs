0e0ed67a020e2785529897ac2b2004995ff2ff90
epicLevi <tomas.carbellido@gmail.com>
2018-01-25 20:58:30 -0600

    Made the Ethan enemies able to shoot front

---
+++ Prototyping/Sketch03/Assets/ControlDisparoEnemigo.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControlDisparoEnemigo : MonoBehaviour {
+ 	public GameObject bala;
+
+    // Use this for initialization
+    void Start () {
+		InvokeRepeating("ShootProjectile", 2.0f, 2.0f);
+	}
+	
+	void ShootProjectile() {
+		GameObject instance = Instantiate(bala, this.transform.position, this.transform.rotation) as GameObject;
+        instance.GetComponent<Rigidbody>().AddForce(-this.transform.forward * 10000);
+	}
+}
